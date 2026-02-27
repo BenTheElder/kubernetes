8d5704385e43ea1be9776b239b963023164bbb47
Anish Ramasekar <anish.ramasekar@gmail.com>
2026-02-27 13:49:51 -0800

    Add ManifestBasedAdmissionControlConfig feature gate
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2072,6 +2072,10 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
+	genericfeatures.ManifestBasedAdmissionControlConfig: {
+		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+	},
+
 	genericfeatures.MutatingAdmissionPolicy: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.34"), Default: false, PreRelease: featuregate.Beta},
@@ -2555,6 +2559,8 @@ var defaultKubernetesFeatureGateDependencies = map[featuregate.Feature][]feature
 	genericfeatures.KMSv1: {},
 
 	genericfeatures.ListFromCacheSnapshot: {},
+
+	genericfeatures.ManifestBasedAdmissionControlConfig: {},
 
 	genericfeatures.MutatingAdmissionPolicy: {},
 
